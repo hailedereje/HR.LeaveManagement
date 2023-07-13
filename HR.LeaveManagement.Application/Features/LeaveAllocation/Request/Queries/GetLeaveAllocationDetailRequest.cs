9dd11db4bf10cda60eb9ac2989ae278c3a76a5f7
hailedereje <hailedereje88@gmail.com>
2023-07-13 15:29:38 +0300

    implementing MediatoR for features

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Request/Queries/GetLeaveAllocationDetailRequest.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Request.Queries
+{
+    public class GetLeaveAllocationDetailRequest : IRequest<LeaveAllocationDTO>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Handlers/Queries/GetLeaveTypeRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.Features.LeaveType.Requests.Queries;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Handlers.Queries
+{
+    public class GetLeaveTypeRequestHandler : IRequestHandler<GetLeaveTypeRequest, List<LeaveTypeDTO>>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+        public GetLeaveTypeRequestHandler(ILeaveTypeRepository leaveTypeRepository , IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<LeaveTypeDTO>> Handle(GetLeaveTypeRequest request, CancellationToken cancellationToken)
+        {
+            var leaveTypes = await _leaveTypeRepository.GetAll();
+            return _mapper.Map<List<LeaveTypeDTO>>(leaveTypes);
+        }
+    }
+}
